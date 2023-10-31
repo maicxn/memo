e153370e3171f931ffd869ba5662c743cc164f55
maicxn <maiconkelbert@gmail.com>
2023-10-31 01:47:04 -0300

    feat: :art: first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CreateThoughtComponent } from './components/thought/create-thought/create-thought.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    CreateThoughtComponent
   ],
   imports: [
     BrowserModule,
